9f6b8133061ec36abdad27748341cdd955ca61fa
LUKE TRUITT <lot@duke.edu>
2020-09-04 17:57:09 -0400

    dist

--- spell_check.py
+++ spell_check.py
@@ -14,8 +14,10 @@
 # You may work in a group of 1 or 2. Submissions will be graded without
 # regard for the group size. Submit your solution in a .zip file including a
 # Jupyter notebook (.ipynb file) demonstrating its usage.
+import numpy as np
 
-DICT_DATA = "dict.txt"
+DICT_DATA = "dict2.txt"
+
 
 def init_dict(FILE_NAME):
     f = open(FILE_NAME, "r")
@@ -24,6 +26,38 @@ def init_dict(FILE_NAME):
         dict_set.add(word.split('\n')[0])
     return dict_set
 
+
+def calc_dist(A, B):
+
+    # Initialize matrix
+    xs = len(A)+1
+    ys = len(B)+1
+    grid = np.zeros((xs, ys), dtype=int)
+
+    # Make the yumns
+    for i in range(1, xs):
+        for j in range(1, ys):
+            grid[i][0] = i
+            grid[0][j] = j
+
+    # Find cost of deletions,insertions and/or substitutions
+    for x in range(1, xs):
+        for y in range(1, ys):
+            if A[x-1] == B[y-1]:  # Letters on the same
+                cost = 0
+            else:
+                cost = 1
+            grid[x][y] = min(grid[x-1][y] + 1,      # Cost of deletions
+                             grid[x][y-1] + 1,          # Cost of insertions
+                             grid[x-1][y-1] + cost)     # Cost of substitutions
+
+    print(grid[x][y])
+
+
 if __name__ == "__main__":
     word_dict = init_dict(DICT_DATA)
-    print(word_dict)
+    # print(word_dict)
+    A = 'Apple Inc.'
+    B = 'apple Inc'
+
+    calc_dist(A.lower(), B.lower())
