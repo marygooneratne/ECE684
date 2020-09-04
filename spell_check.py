d87bf4013cf29518cc15f78e1c6499ef60a16930
Mary Gooneratne <mmg53@duke.edu>
2020-09-04 17:26:02 -0400

    Added inst

---
+++ spell_check.py
@@ -0,0 +1,16 @@
+# Build from scratch a spelling corrector in Python. It should include:
+#   1. tokenization
+#   2. edit distance-based non-word spelling correction
+#   3. de-tokenization
+# As an example use case, consider a version of Jane Austen’s Sense and
+# Sensibility (available via nltk’s gutenberg corpus) corrupted by random insertions,
+# deletions, and substitutions.
+# Constraints:
+#   • Your spelling correction function should accept a single string and return
+#   a single string.
+#   • Your spelling correction module may use only standard libraries and
+#   numpy.
+#   • To test it you may use the nltk.corpus package.
+# You may work in a group of 1 or 2. Submissions will be graded without
+# regard for the group size. Submit your solution in a .zip file including a
+# Jupyter notebook (.ipynb file) demonstrating its usage.
